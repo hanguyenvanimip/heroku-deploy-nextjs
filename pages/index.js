1b625df317f172cf03f3dae47dac331c79a86548
Nguyen Van Ha <hanv@tiin.vn>
2019-10-11 13:28:17 +0700

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import About from "../componnets/About";
 const Index = () => (
     <div>
-    <p>Hello World!</p>
-</div>
+        <p>Hello World!</p>
+        <About/>
+    </div>
 )
 export default Index;
